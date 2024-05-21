18c4d15d0168962c9b4c6c51ac5a40fae53d923a
Rahmat Nur <v-rahmat.nur@dana.id>
2024-05-22 01:18:44 +0700

    perbaiki menu interaksi

--- node-glints/server.js
+++ node-glints/server.js
@@ -20,13 +20,36 @@ const port = PORT|| 3000;
 const RespondBuilderText=(body)=>{
   let result;
   if(body==="1"||body===1){
-    result="Menu 1"
+    result = "*1. Dimana lokasi Absensi seharusnya?*\n\n\
+      Lokasi mengambil absen yang dibenarkan sekolah adalah  :\n\
+      \t -Di depan Kelas \n\
+      \t -Di Gerbang Sekolah\n\
+      \t -Dan Dilapangan upacara\n\
+    ";
   }
   else if (body === "2" || body === 2) {
-    result = "Menu 2"
+    result = "*2. Sudah berapa kali pelanggaran kamu?*\n\n\
+      pelanggaran kamu baru 3 kali nih  :\n\
+      \t -Terlambat masuk pada pukul 08:20 Kamis 24 April 2024\n\
+      \t -Terlalu cepat pulang pada pukul 12:20 Senin 27 April 2024\n\
+      \t -Terlambat masuk pada pukul 07:20 Sabtu 1 mei 2024\n\
+    ";
+  }
+  else if (body === "3" || body === 3) {
+    result = "*3. Jadwal mata pelajaran hari ini.*\n\n\
+      jadwal hari ini adalah:\n\
+      \t -Matematika 08:20\n\
+      \t -Bahasa Indonesia\n\
+      \t -Terlambat masuk pada pukul 07:20 Sabtu 1 mei 2024\n\
+    ";
+  }
+  else if (body === "4" || body === 4) {
+    result = "*4. Hukuman apa saja yang kamu terima jika telat mengambil absen?*\n\n\
+      Hukuman yang akan kamu terima jika telat adalah hukuman PANCUNG wkwkwk\n\
+    ";
   }
   else{
-    result = "Unfied meaning!"
+    result = "Undified respond!"
   }
 
   return result;
