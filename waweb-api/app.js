4d265cec0ed0c5bee9c3f31e121da88e8a06ae94
Rahmat Nur <v-rahmat.nur@dana.id>
2024-05-22 18:59:00 +0700

    perbaiki errror pada script import express js

--- waweb-api/app.js
+++ waweb-api/app.js
@@ -1,19 +1,19 @@
-import express from "express";
-import dotenv from "dotenv";
-import routes from "./routes.js"
-import { logRequests } from "./middleware.js"
+import express from 'express';
+import dotenv from 'dotenv';
+import routes from './routes.js';
+import { logRequests } from './middleware.js';
+
 dotenv.config();
-const app=express();
+
+const app = express();
 app.use(express.json());
 app.use(logRequests);
 
-const {PORT}=process.env;
-const port =PORT||3000;
+const { PORT } = process.env;
+const port = PORT || 3000;
 
 app.use(routes);
 
-app.listen(port,()=>{
-  console.log('====================================');
-  console.log(`Server is listening on port : {$port}`);
-  console.log('====================================');
-})
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
