bc8590f5cb713fab8d0f36ef4d0c713629e0eb85
Rahmat Nur <v-rahmat.nur@dana.id>
2024-06-05 22:39:20 +0700

    create user with modals successed

--- frontend-absekol/front-crud1/main.js
+++ frontend-absekol/front-crud1/main.js
@@ -1,10 +1,29 @@
 const app = Vue.createApp({
     data:()=>{
         return{
-            users:[]
+            input: {
+                email: 'email',
+                username: 'username',
+                password: 'p',
+                noWa: '00',
+                roleId: 3
+            },
+            users:[],
+            modalCreate:false
         }
     },
     methods:{
+        async createUser(){
+            try {
+                // const respong
+                const response = await axios.post('https://absekol-api.numpang.my.id/api/users',this.input); // Ganti dengan endpoint API Anda
+                this.fetchUsers();
+                this.closeModal();
+            } catch (error) {
+                console.error('Error create user:', error);
+            }
+        }
+        ,
         async fetchUsers() {
             try {
                 const response = await axios.get('https://absekol-api.numpang.my.id/api/users'); // Ganti dengan endpoint API Anda
@@ -12,6 +31,17 @@ const app = Vue.createApp({
             } catch (error) {
                 console.error('Error fetching users:', error);
             }
+        },
+        async deleteUser(uid) {
+            try {
+                await axios.delete(`https://absekol-api.numpang.my.id/api/users/${uid}`); // Ganti dengan endpoint API Anda
+                this.fetchUsers(); // Refresh user list
+            } catch (error) {
+                console.error('Error deleting user:', error);
+            }
+        },
+        closeModal() {
+            document.getElementById('closeModalCreat').click();
         }
     },
     mounted() {
