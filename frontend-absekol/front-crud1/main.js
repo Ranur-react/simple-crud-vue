b0459663578f003d3c7affcfd861898ba9a6c6d4
Rahmat Nur <v-rahmat.nur@dana.id>
2024-06-06 21:49:51 +0700

    form validations

--- frontend-absekol/front-crud1/main.js
+++ frontend-absekol/front-crud1/main.js
@@ -9,7 +9,39 @@ const app = Vue.createApp({
                 roleId: 3
             },
             users:[],
-            modalCreate:false
+            modalCreate:false,
+            formValidations:{
+                email: {
+                    errorMessage: null,
+                    validMessage: "Looks good!",
+                    isValid:true,
+                    isInvalid:null
+
+                },
+                username: {
+                    errorMessage: null,
+                    validMessage: "Looks good!",
+                    isValid: true,
+                    isInvalid: null
+
+                },
+                password: {
+                    errorMessage: null,
+                    validMessage: "Looks good!",
+                    isValid: null,
+                    isInvalid: null
+
+                },
+                noWa: {
+                    errorMessage: null,
+                    validMessage: "Looks good!",
+                    isValid: null,
+                    isInvalid: null
+
+                },
+                httpRespondCode: null,
+
+            }
         }
     },
     methods:{
@@ -20,6 +52,48 @@ const app = Vue.createApp({
                 this.fetchUsers();
                 this.closeModal();
             } catch (error) {
+                if (error.response){
+                    const result = error.response;
+                    if(result.status==500){
+                        this.formValidations.httpRespondCode=result.status;
+                        if (result.data.errorDetails){
+                            if (result.data.errorDetails.errors.length>0){
+                                result.data.errorDetails.errors.map((items,key)=>{
+                                    console.log("----------Value----------------")
+                                    console.log(items)
+                                    console.log("-----------Key---------------")
+                                    console.log(key)
+                                    if(items.path=="email"){
+                                        this.formValidations.email.errorMessage = items.message;
+                                        this.formValidations.email.isInvalid = true;
+                                        this.formValidations.username.isInvalid = false;
+
+                                    } else if (items.path == "username") {
+                                        this.formValidations.username.errorMessage = items.message;
+                                        this.formValidations.username.isInvalid = true;
+                                        this.formValidations.email.isInvalid = false;
+
+                                    } else if (items.path == "noWa") {
+                                        this.formValidations.noWa.errorMessage = items.message;
+                                        this.formValidations.noWa.isInvalid = true;
+                                        this.formValidations.username.isInvalid = false;
+                                        this.formValidations.email.isInvalid = false;
+
+                                    } else if (items.path == "password") {
+                                        this.formValidations.password.errorMessage = items.message;
+                                        this.formValidations.password.isInvalid = true;
+                                        this.formValidations.noWa.isInvalid = false;
+                                        this.formValidations.username.isInvalid = false;
+                                        this.formValidations.email.isInvalid = false;
+
+                                    }
+
+
+                                })
+                            }
+                        }
+                    }
+                }
                 console.error('Error create user:', error);
             }
         }
